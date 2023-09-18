56b6e1e2258a26098b883dae6deec51c9120358f
Thorben Below <56894536+thorbenbelow@users.noreply.github.com>
2023-09-18 21:26:02 +0200

    add basic button example
    Signed-off-by: Thorben Below <56894536+thorbenbelow@users.noreply.github.com>

--- server.go
+++ server.go
@@ -4,14 +4,43 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"log"
+	"html/template"
 )
+
+var db Db
+
+type Db struct {
+	users []string
+};
+
+func initDb() Db {
+	return Db{users: make([]string, 0)}	
+}
+
+func (db *Db) addUser(mail string) error {
+	db.users = append(db.users, mail)
+	return nil
+}
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("public/*")
+	db = initDb()
+	if err := db.addUser("test@example.invalid"); err != nil {
+		log.Fatal("You suck")
+	}
+	
+	router.LoadHTMLGlob("templates/*")
+	btn := template.HTML(`
+	<button hx-post="/user"">
+		Click Me
+	</button>`)
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{ "msg": "Hello World"})
+		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{ "msg": btn})
 	})
-	log.Fatal(router.Run(":8080"))
 
+	router.POST("/user", handlePostUser)
+	log.Fatal(router.Run(":8080"))
+}
+func handlePostUser(ctx *gin.Context) {
+	log.Print("yay")
 }
