32ebb5729844f44f7433ec13df381f8aebb2176f
Thorben Below <56894536+thorbenbelow@users.noreply.github.com>
2023-09-18 21:58:28 +0200

    add basic data rendering
    Signed-off-by: Thorben Below <56894536+thorbenbelow@users.noreply.github.com>

--- server.go
+++ server.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"log"
 	"html/template"
+	"log"
+	"net/http"
 )
 
 var db Db
 
 type Db struct {
 	users []string
-};
+}
 
 func initDb() Db {
-	return Db{users: make([]string, 0)}	
+	return Db{users: make([]string, 0)}
 }
 
 func (db *Db) addUser(mail string) error {
@@ -25,22 +25,30 @@ func (db *Db) addUser(mail string) error {
 func main() {
 	router := gin.Default()
 	db = initDb()
-	if err := db.addUser("test@example.invalid"); err != nil {
-		log.Fatal("You suck")
-	}
-	
+
 	router.LoadHTMLGlob("templates/*")
-	btn := template.HTML(`
-	<button hx-post="/user"">
+	newUserForm := template.HTML(`
+		<form hx-post="/user" hx-swap="none">
+		<input type="text" placeholder="Enter new user" name="user">
+	<button type="submit" >
 		Click Me
-	</button>`)
+	</button> </form>`)
+
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{ "msg": btn})
+		ctx.HTML(http.StatusOK, "index.tmpl", map[string]interface{}{"newUserForm": newUserForm, "userList": db.users})
 	})
 
 	router.POST("/user", handlePostUser)
 	log.Fatal(router.Run(":8080"))
 }
+
+type NewUser struct {
+	User string `form:"user"`
+}
+
 func handlePostUser(ctx *gin.Context) {
-	log.Print("yay")
+	var newUser NewUser
+	ctx.Bind(&newUser)
+	db.addUser(newUser.User)
+	log.Print(newUser)
 }
