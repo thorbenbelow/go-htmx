a7a25550a252457e090d385b78a051b7b8cc576c
Thorben Below <56894536+thorbenbelow@users.noreply.github.com>
2023-09-20 18:35:29 +0200

    add sqlite3
    Signed-off-by: Thorben Below <56894536+thorbenbelow@users.noreply.github.com>

--- server.go
+++ server.go
@@ -1,50 +1,93 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gin-gonic/gin"
-	"html/template"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 )
 
-var db Db
-
 type Db struct {
-	users []string
+	pool *sql.DB
 }
 
-func initDb() Db {
-	return Db{users: make([]string, 0)}
+func initDb(path string) (*Db, error) {
+	pool, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := pool.Exec(`CREATE TABLE IF NOT EXISTS users (id integer not null primary key autoincrement, name text);`); err != nil {
+		return nil, err
+	}
+
+	return &Db{pool}, nil
 }
 
 func (db *Db) addUser(mail string) error {
-	db.users = append(db.users, mail)
+	stmt, err := db.pool.Prepare(`insert into users(name) values(?)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	if _, err := stmt.Exec(mail); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (db *Db) getUsers() ([]string, error) {
+	rows, err := db.pool.Query(`SELECT name FROM users;`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	res := make([]string, 0)
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return nil, err
+		}
+		log.Print(name)
+		res = append(res, name)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	log.Print(res)
+	return res, nil
 }
 
 func main() {
 	router := gin.Default()
-	db = initDb()
+	db, err := initDb("./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	router.LoadHTMLGlob("templates/*")
-	newUserForm := template.HTML(`
-		`)
-
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.tmpl", map[string]interface{}{"newUserForm": newUserForm, "userList": db.users})
+		users, err := db.getUsers()
+		if err != nil {
+			log.Print(err)
+		}
+		ctx.HTML(http.StatusOK, "index.tmpl", map[string]interface{}{"newUserForm": "", "userList": users})
 	})
 
-	router.POST("/user", handlePostUser)
+	router.POST("/user", func(ctx *gin.Context) {
+		var newUser NewUser
+		ctx.Bind(&newUser)
+		db.addUser(newUser.User)
+		users, err := db.getUsers()
+		if err != nil {
+			log.Print(err)
+		}
+		ctx.HTML(http.StatusOK, "UserList.tmpl", gin.H{"userList": users})
+	})
 	log.Fatal(router.Run(":8080"))
 }
 
 type NewUser struct {
 	User string `form:"user"`
-}
-
-func handlePostUser(ctx *gin.Context) {
-	var newUser NewUser
-	ctx.Bind(&newUser)
-	db.addUser(newUser.User)
-	ctx.HTML(http.StatusOK, "UserList.tmpl", gin.H{"userList": db.users})
 }
